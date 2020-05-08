e39ff625bb1ecbb844c4384fefe598b77a8c5d85
Engin YILMAZ <engin.yilmaaz@gmail.com>
2020-05-08 07:21:18 +0300

    improved some features

--- Windows Shutdown Helper/settingsForm.Designer.cs
+++ Windows Shutdown Helper/settingsForm.Designer.cs
@@ -257,7 +257,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(457, 390);
+            this.ClientSize = new System.Drawing.Size(457, 375);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
